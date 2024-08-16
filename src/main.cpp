d1bbf197e32ccfe4d6235da0a62399dab2b9956f
Reggulaar <jamp.ivonin@bk.ru>
2024-08-16 21:18:23 +0300

    feat(PIN): train of print and read signals

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,13 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
-
 void setup() {
   Serial.begin(9600);
   Serial.available();
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
-  Serial.println(result);
-
+  pinMode(7, OUTPUT);
+  pinMode(6, INPUT_PULLUP);
 }
 
 void loop() {
-    String hello = Serial.readString();
-    Serial.print(hello);
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  digitalWrite(7, HIGH);
+  Serial.print(digitalRead(7)); Serial.println(!digitalRead(6));
 }
