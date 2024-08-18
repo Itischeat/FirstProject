6bf1e80a4ac033d39330f216c641e4be5c0b91d9
Reggulaar <jamp.ivonin@bk.ru>
2024-08-18 17:08:36 +0300

    feat(train): Function Declaration && Timer function

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,30 @@
 #include <Arduino.h>
 
+// uint32_t timer;
+// uint32_t inter;
+
+// uint32_t getInterval();
+
 void setup() {
   Serial.begin(9600);
   Serial.available();
-  pinMode(7, OUTPUT);
-  pinMode(6, INPUT_PULLUP);
-}
+  // pinMode(13, OUTPUT);
+  // inter = 100;
+};
 
 void loop() {
-  digitalWrite(7, HIGH);
-  Serial.print(digitalRead(7)); Serial.println(!digitalRead(6));
-}
+
+  // Serial.println(inter);
+  // if (getInterval() >= inter && inter != 0) {
+  //   timer = millis();
+  //   digitalWrite(13, HIGH);
+  //   delay(35);
+  // };
+
+  // digitalWrite(13, LOW);
+};
+
+// uint32_t getInterval() {
+//     return millis() - timer;
+// };
+
